66e99af9bb57d8ef48b6750974c8a4407d715e2a
Evan Overly <evanhoverly@gmail.com>
2024-05-21 17:12:31 -0500

    Added random spotlight

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import CardGrid from './components/CardGrid';
+import Card from './components/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import './styles.css';
 import projectData from './data/projects.json';
 
 function App() {
+  let randomIndex = Math.floor(Math.random() * projectData.length);
+  let randomCardData = projectData[randomIndex];
+  console.log(randomCardData);
+
   return (
     <div className="App">
       <h1>Evan Overly</h1>
@@ -19,6 +24,12 @@ function App() {
       </div>
 
       <hr />
+      <h2>Have tried this?</h2>
+      <div className='container'>
+        <div className='row'>
+          <Card data={randomCardData} />
+        </div>
+      </div>
       <h2>Previous Works</h2>
       <CardGrid data={projectData} />
 
